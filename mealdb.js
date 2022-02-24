73d4459b864c1232cc27e6eef7e3c23f28720519
MdAnowar1 <anowarplus1014@gmail.com>
2022-02-24 23:50:05 +0600

    meal deatils3

--- mealdb.js
+++ mealdb.js
@@ -1,8 +1,10 @@
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value ;
-    // console.log(searchText);
+    // clear data 
     searchField.value = '';
+
+    // load data 
     const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     // console.log(url);
     fetch(url)
@@ -13,6 +15,8 @@ const searchFood = () => {
 
 const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
+    // searchResult.innerHTML = '';
+    searchResult.textContent = '';
     meals.forEach(meal => {
         // console.log(meal);
         // console.log(meal);
@@ -41,4 +45,16 @@ const loadMealDetail = mealId => {
 
 const displayMealDetail = meal => {
     console.log(meal);
+    const mealDetails = document.getElementById('meal-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${meal.strMeal}</h5>
+      <p class="card-text">${meal.strInstructions.slice(0,150)}</p>
+      <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `;
+    mealDetails.appendChild(div);
 }
